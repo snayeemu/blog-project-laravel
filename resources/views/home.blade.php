446ac7054d8c0a0da3b4a10a1f8e2199a8adb622
snayeemu <snayeemu2@gmail.com>
2023-12-25 16:50:11 +0600

    registration and login form updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
 
@@ -41,7 +42,7 @@
   </div>
 
   @else
-  <div style="border: 3px solid black;">
+  {{-- <div style="border: 3px solid black;">
     <h2>Register</h2>
     <form action="/register" method="POST">
       @csrf
@@ -50,8 +51,31 @@
       <input name="password" type="password" placeholder="password">
       <button>Register</button>
     </form>
+  </div> --}}
+  
+
+  <div class="w-50 mx-auto border border-black rounded p-2 m-3">
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+        @csrf
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Your name</label>
+          <input type="text" name="name" class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" name='password' class="form-control" id="exampleInputPassword1">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Register</button>
+      </form>
   </div>
-  <div style="border: 3px solid black;">
+
+  {{-- <div style="border: 3px solid black;">
     <h2>Login</h2>
     <form action="/login" method="POST">
       @csrf
@@ -59,9 +83,30 @@
       <input name="loginpassword" type="password" placeholder="password">
       <button>Log in</button>
     </form>
+  </div> --}}
+
+  <div class="w-50 mx-auto border border-black rounded p-2 m-3">
+    <h2>Register</h2>
+    <form action="/login" method="POST">
+        @csrf
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Username</label>
+          <input type="text" name="loginname" class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+          
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" name='loginpassword' class="form-control" id="exampleInputPassword1">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Register</button>
+      </form>
   </div>
+
   @endauth
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   
 </body>
 </html>
